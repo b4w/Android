404b618e986d8dd8234863f74024aa14c14c1043
b4w <dzirtdurden@gmail.com>
2014-10-28 00:22:07 +0300

    add new project-folder devColibri->add new simple project Hello World. Modified project ToDoList

---
+++ proAndroid4/ToDoList/app/src/main/java/com/proAndroid4/todolist/NewItemFragment.java
@@ -0,0 +1,18 @@
+//package com.proAndroid4.todolist;
+//
+//import android.app.Fragment;
+//import android.os.Bundle;
+//import android.support.annotation.Nullable;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//
+//public class NewItemFragment extends Fragment {
+//    @Nullable
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+//        return inflater.inflate(R.layout.new_item_fragment, container, false);
+//    }
+//}
+//
+//
