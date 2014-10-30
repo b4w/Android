60d854df461df73c608b0dc93dfd99f942b8987b
b4w <dzirtdurden@gmail.com>
2014-10-30 12:50:35 +0300

    add new project ListViewExam. Work with ListView, system ArrayAdapter and customer Adapter with pojo class

---
+++ devColibri/ListViewExam/app/src/main/java/com/devcolibri/listviewexam/app/pojo/PhoneModel.java
@@ -0,0 +1,38 @@
+package com.devcolibri.listviewexam.app.pojo;
+
+public class PhoneModel {
+    
+    private long id;
+    private String name;
+    private long price;
+
+    public PhoneModel(long id, String name, long price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getPrice() {
+        return price;
+    }
+
+    public void setPrice(long price) {
+        this.price = price;
+    }
+}
