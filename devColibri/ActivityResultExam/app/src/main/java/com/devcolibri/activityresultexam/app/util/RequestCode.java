4ba5dc2887b3083ead369184fcde14eb5c376832
b4w <dzirtdurden@gmail.com>
2016-03-27 10:37:21 +0300

    Add test project - AppRestService

--- devColibri/ActivityResultExam/app/src/main/java/com/devcolibri/activityresultexam/app/util/RequestCode.java
+++
@@ -1,8 +0,0 @@
-package com.devcolibri.activityresultexam.app.util;
-
-public class RequestCode {
-
-    public static final int REQUEST_CODE_PRESENTED = 1;
-    public static final int REQUEST_CODE_LANGUAGE = 2;
-
-}
---
+++ devColibri/AndroidAppRestService/AndroidAppRestService/app/src/main/java/com/android_app_rest_service/constantine/androidapprestservice/MainActivity.java
@@ -0,0 +1,35 @@
+package com.android_app_rest_service.constantine.androidapprestservice;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+/**
+ * Наследуясь от AppCompatActivity, мы сможем использовать все наши компоненты из designLibrary.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppThemeDefault);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+        toolbar.inflateMenu(R.menu.menu);
+    }
+}
